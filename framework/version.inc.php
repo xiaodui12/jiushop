0f7c23dd3f239bb36014a5160b8a1437ced156ed
linchuanxiao <952979483@qq.com>
2019-09-28 12:39:53 +0800

    测试

---
+++ framework/version.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 版本号
+ *
+ * [WeEngine System] Copyright (c) 2013 www.efwww.com
+ */
+
+defined('IN_IA') or exit('Access Denied');
+
+define('IMS_FAMILY', 'v');
+define('IMS_VERSION', '2.9.0');
+define('IMS_RELEASE_DATE', '20180917');
